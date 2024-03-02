fe3db94072efe8caacae4acec45c9fb54fc740ea
MuMu3an <wangyoubin1205@gmail.com>
2024-03-02 02:31:38 -0800

    First Update Version 2

---
+++ Assets/Scripts/Lines - Version 2/TeleportLine.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportLine : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        if (player != null)
+        {
+            Vector3 teleportPosition = transform.position; 
+            player.transform.position = teleportPosition;
+        }
+    }
+}
