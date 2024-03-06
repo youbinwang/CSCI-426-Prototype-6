684ac2e1cbbabae264c4252374fab04a147411ea
MuMu3an <wangyoubin1205@gmail.com>
2024-03-06 00:23:37 -0800

    Teleport Line Modify

--- Assets/Scripts/Lines - Version 2/TeleportLine.cs
+++ Assets/Scripts/Lines - Version 2/TeleportLine.cs
@@ -10,25 +10,25 @@ public class TeleportLine : MonoBehaviour
     public LineSpawner2 spawner;
     private void Start()
     {
-       /* GameObject player = GameObject.Find("===Player===");
+        GameObject player = GameObject.Find("===Player===");
         if (player != null)
         {
             Vector3 teleportPosition = transform.position; 
             player.transform.position = teleportPosition;
-        }*/
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            if (!haveUsed)
-            {
-                //collision.gameObject.transform.position = spawner.circle2Position.position;
-                Vector3 teleportPosition = transform.position;
-                collision.gameObject.transform.position = teleportPosition;
-                haveUsed = true;
-            }
         }
     }
+
+    // private void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     if (collision.gameObject.tag == "Player")
+    //     {
+    //         if (!haveUsed)
+    //         {
+    //             //collision.gameObject.transform.position = spawner.circle2Position.position;
+    //             Vector3 teleportPosition = transform.position;
+    //             collision.gameObject.transform.position = teleportPosition;
+    //             haveUsed = true;
+    //         }
+    //     }
+    // }
 }
